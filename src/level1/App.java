b30e8a116b3ab82f7f238335541e7d4a59794703
jay1261 <dongjae73@naver.com>
2024-04-23 11:13:25 +0900

    4. 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.

--- src/level1/App.java
+++ src/level1/App.java
@@ -6,30 +6,37 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int firstNum = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int secondNum = sc.nextInt();
-        sc.nextLine();
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        String operator = sc.nextLine();
-        double result = switch (operator) {
-            case "+" -> firstNum + secondNum;
-            case "-" -> firstNum - secondNum;
-            case "*" -> firstNum * secondNum;
-            case "/" -> {
-                if (secondNum == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    yield 0;
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int firstNum = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int secondNum = sc.nextInt();
+            sc.nextLine();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String operator = sc.nextLine();
+            double result = switch (operator) {
+                case "+" -> firstNum + secondNum;
+                case "-" -> firstNum - secondNum;
+                case "*" -> firstNum * secondNum;
+                case "/" -> {
+                    if (secondNum == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        yield 0;
+                    }
+                    yield (double) firstNum / secondNum;
                 }
-                yield (double) firstNum / secondNum;
+
+                default -> 0;
+            };
+            System.out.println("결과: " + result);
+
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
+            String answer = sc.nextLine();
+            if (answer.equals("exit")){
+                break;
             }
-
-            default -> 0;
-        };
-        System.out.println("결과: " + result);
-
+        }
     }
 }
